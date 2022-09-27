cb863cf0bd09449556cba4b98e107b75d16d892e
kimziou77 <kimziou77@naver.com>
2022-09-27 17:03:35 +0900

    init : project setting (#6)

---
+++ gagebu/src/test/java/com/payhere/gagebu/GagebuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.payhere.gagebu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GagebuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
