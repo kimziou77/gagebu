4e46ec389fc327b7267fda6ef8dae337103f1a2e
kimziou77 <kimziou77@naver.com>
2022-09-27 17:03:35 +0900

    init : project setting (#6)

---
+++ gagebu/src/main/java/com/payhere/gagebu/GagebuApplication.java
@@ -0,0 +1,13 @@
+package com.payhere.gagebu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GagebuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GagebuApplication.class, args);
+    }
+
+}
