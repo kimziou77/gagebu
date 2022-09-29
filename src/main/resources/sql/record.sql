43e50429bab9889b488436dd68400f47dbcfd5cc
kimyo <41179265+kimziou77@users.noreply.github.com>
2022-09-29 20:04:36 +0900

    docs: readme.md (#26)

--- src/main/resources/sql/record.sql
+++ src/main/resources/sql/record.sql
@@ -1,14 +1,14 @@
 create table record
 (
-    id         bigint       not null primary key,
-    created_at datetime(6)  null,
-    updated_at datetime(6)  null,
-    deleted    bit          not null,
-    memo       varchar(255) null,
-    money      int          null,
-    name       varchar(255) null,
-    user_id    bigint       null,
-    constraint FK44ctj7m4iik9qhcbaqt1aynka
-        foreign key (user_id) references users (id)
+    id         bigint      not null primary key,
+    created_at datetime(6) null,
+    updated_at datetime(6) null,
+    deleted    bit         not null,
+    deleted_at datetime(6) null,
+    memo       varchar(60) not null,
+    money      int         not null,
+    name       varchar(20) not null,
+    user_id    bigint      not null,
+    constraint FK44ctj7m4iik9qhcbaqt1aynka foreign key (user_id) references users (id)
 );
 
--- src/main/resources/sql/users.sql
+++ src/main/resources/sql/users.sql
@@ -1,13 +1,11 @@
 create table users
 (
-    id         bigint       not null
-        primary key,
+    id         bigint       not null primary key,
     created_at datetime(6)  null,
     updated_at datetime(6)  null,
     email      varchar(255) null,
-    password   varchar(255) null,
+    password   varchar(200) null,
     user_role  varchar(255) null,
-    constraint UK_6dotkott2kjsp8vw4d0m25fb7
-        unique (email)
+    verified   bit          not null,
+    constraint UK_6dotkott2kjsp8vw4d0m25fb7 unique (email)
 );
-
